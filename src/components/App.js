3c9ffbe4aea8d620198b313d00521bfdb4a8ad52
Igor <Igor.panggabean@rithm.nl>
2017-10-31 12:34:49 +0700

    init data from internal array

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,39 @@ import React, { Component } from 'react';
 import '../App.css';
 import TodoList from './TodoList';
 
+var data = [
+  "Task 1",
+  "Task 2",
+  "Task 3"
+]
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myLists: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      myLists: data
+    })
+  }
+
   render() {
+    var renderList = this.state.myLists.map(function(item, index) {
+      return(
+        <TodoList key={index} item={item}/>
+      )
+    })
+
     return (
       <div>
         <h1>List Task</h1>
         <h3>Todo:</h3>
         <ul className="list-unstyled">
-          <TodoList item="Task 1"/>
-          <TodoList item="Task 2"/>
-          <TodoList item="Task 3"/>
+          {renderList}
         </ul>
       </div>
     );
