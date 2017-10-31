ea36e4f01044df3449c74d776afe35d124b7ec75
Igor <Igor.panggabean@rithm.nl>
2017-10-31 12:21:24 +0700

    code-skeleton

--- src/App.js
+++ src/App.js
@@ -4,33 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>List Task</h1>
-        <h3>Todo:</h3>
-        <ul className="list-unstyled">
-          <li>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"/> Task 1
-              </label>
-            </div>
-          </li>
-          <li>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"/> Task 2
-              </label>
-            </div>
-          </li>
-          <li>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox"/> Task 3
-              </label>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <div>Hello World</div>
     );
   }
 }
